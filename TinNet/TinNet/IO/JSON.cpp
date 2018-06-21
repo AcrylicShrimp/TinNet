342b6bfdbb6d845d0522c7959266d7be95debb3d
AcrylicShrimp <led789zxpp@naver.com>
2018-06-22 00:09:13 +0900

    Implementing JSON parser.

---
+++ TinNet/TinNet/IO/JSON.cpp
@@ -0,0 +1,43 @@
+
+/*
+	2018.06.21
+	Created by AcrylicShrimp.
+*/
+
+#include "JSON.h"
+
+namespace TinNet::IO
+{
+	JSONObject JSON::parse(const std::string &sString)
+	{
+
+	}
+
+	std::string &JSON::stringify(const JSONObject &sObject)
+	{
+
+	}
+
+	JSONObject JSON::parseNext(const std::string &sString, std::size_t &nIndex, std::size_t nEndIndex)
+	{
+
+	}
+
+	std::string JSON::parseString(const std::string &sString, std::size_t &nIndex, std::size_t nEndIndex)
+	{
+		if (nIndex >= nEndIndex || sString[nIndex] != '"')
+			return "";
+
+		std::string sResult;
+
+		for (++nIndex; nIndex < nEndIndex && sString[nIndex] != '"'; ++nIndex)
+		{
+			if (sString[nIndex])
+		}
+
+		if (nIndex < nEndIndex && sString[nIndex] == '"')
+			++nIndex;
+
+		return sResult;
+	}
+}
---
+++ TinNet/TinNet/IO/JSONNode.cpp
@@ -0,0 +1,88 @@
+
+/*
+	2018.06.21
+	Created by AcrylicShrimp.
+*/
+
+#include "JSONNode.h"
+
+namespace TinNet::IO
+{
+	/*
+		TODO : Place the static class member variable definitions here.
+	*/
+	
+	
+	JSONNode::JSONNode()
+	{
+		/*
+			TODO : Place the default constructor here.
+		*/
+		
+	}
+	
+	JSONNode::JSONNode(const JSONNode &sSrc)
+	{
+		/*
+			TODO : Place the implementation of the copy constructor here.
+		*/
+		
+	}
+	
+	JSONNode::JSONNode(JSONNode &&sSrc)
+	{
+		/*
+			TODO : Place the implementation of the move constructor here.
+		*/
+		
+	}
+	
+	JSONNode::~JSONNode()
+	{
+		/*
+			TODO : Place the implementation of the destructor here.
+		*/
+		
+	}
+	
+	/*
+		TODO : Place the implementations of other constructors here.
+	*/
+	
+	
+	JSONNode &JSONNode::operator=(const JSONNode &sSrc)
+	{
+		if (&sSrc == this)
+			return *this;
+		
+		/*
+			TODO : Place the implementation of the copy assignment operator here.
+		*/
+		
+		
+		return *this;
+	}
+	
+	JSONNode &JSONNode::operator=(JSONNode &&sSrc)
+	{
+		if (&sSrc == this)
+			return *this;
+		
+		/*
+			TODO : Place the implementation of the move assignment operator here.
+		*/
+		
+		
+		return *this;
+	}
+	
+	/*
+		TODO : Implement other operator overloadings here.
+	*/
+	
+	
+	/*
+		TODO : Place the member function implementations here.
+	*/
+	
+}
